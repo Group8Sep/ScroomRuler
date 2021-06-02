5e611527fb6b247cd04c2183b65c2a3316281094
Tobias van Elk <vanelktobias@gmail.com>
2021-06-02 12:04:19 +0200

    Add project files

---
+++ cmake-build-debug/CMakeFiles/rulertest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rulertest.dir/main.cpp.obj"
+  "librulertest.dll.a"
+  "rulertest.exe"
+  "rulertest.exe.manifest"
+  "rulertest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rulertest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
